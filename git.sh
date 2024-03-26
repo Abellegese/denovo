3cb70f31cc54a9f393b226c9da454227bff57784
Abel Legese SHIBIRU <abellegese@aims.ac.za>
2024-03-27 00:43:09 +0200

    Update model saver

--- git.sh
+++ git.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
 
+# Add all changes
 git add .
-git commit -m "updates"
-git pull origin main
-git push origin main
+
+# Check if a commit message argument is provided
+if [ -z "$1" ]; then
+    # If no commit message is provided, use a default message
+    commit_message="updates"
+else
+    # If a commit message is provided, use it
+    commit_message="$1"
+fi
+
+# Commit changes with the specified message
+git commit -m "$commit_message"
+
+# Push changes to the 'master' branch on the remote repository
+git push -u origin master
